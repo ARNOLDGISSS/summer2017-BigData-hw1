6cbad45c811001f4c63a8670bf7550d5dce60be2
G Mendes <gliffton@gwu.edu>
2017-06-14 11:33:53 -0400

    Problem 3 solution

--- logfile_mapper.py
+++ logfile_mapper.py
@@ -4,7 +4,7 @@
 # Reads whole lines stdin; writes key/value pairs to stdout
 # http://docs.aws.amazon.com/emr/latest/ReleaseGuide/UseCase_Streaming.html
 # --- DO NOT MODIFY ANYTHING ABOVE THIS LINE ---
-
+import re
 import sys
 import time
 import datetime
@@ -12,6 +12,7 @@ import datetime
 for line in sys.stdin:
 	line = line.rstrip()
 	words = line.split('[')
-	words = words[1].split(' -0800')
+	#words = words[1].split(' -0800')
+	words = re.split(r' -0800| -0700',words[1])
 	time = datetime.datetime.strptime(words[0], "%d/%b/%Y:%H:%M:%S")
 	print(time.strftime('%Y-%m')+"\t1")
